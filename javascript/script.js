4527986f8a34f4516e67271983927e53c35d6e2b
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-10-08 14:03:34 -0700

    updating the javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,45 @@
-var APIKey = "aecc33e1f3ac606321129d5660f649cb";
-var city = document.getElementById('cityName');
+$(document).readyu(function() {
 
 
-
+    var APIKey = "aecc33e1f3ac606321129d5660f649cb";
 //var queryURL = "api.openweathermap.org/data/2.5/weather?q=city&appid=APIKey;
 var input = document.getElementById('city');
 
-function getWeather() {
-    createCanvas(400, 200);
-    loadJSON('api.openweathermap.org/data/2.5/weather?q=London&appid=APIKey',data)
-    var button= select('#search');
-    button.mousePressed(weatherRequest);
-    input = select('#city');
-}
-
-function weatherRequest() {
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + input.value() + "&appid=" + APIKey;
-
-loadJSON(queryURL, requestMade);
-
-
- 
-}
-
-function requestMade(data) {
-    console.log(data);
-    console.log(weather);
-    weather = data;
-
-
-
+//current weather fetch
+function fetchWeather(cityName, callback) {
+    var CurrentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
+    $.getJSON(CurrentWeatherURL, callback);
 }
 
 
+//5 day forcast fetch
+function ForecastData(cityName, callback) {
+    var futureForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
+    $.getJSON(futureForecast, callback);
+}
+
+
+function currentWeather(data) {
+    var city = data.name;
+    var date = dayjs().format('MMMM D, YYYY') 
+    var weatherIcon = a.weather[0].weatherIcon
+    var weatherURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
