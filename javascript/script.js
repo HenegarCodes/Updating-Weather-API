c408c37c6193b8b42525b16d8b05fd01ca837677
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-10-09 15:36:06 -0700

    added the localstorage to store as a string the search

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 
 
     var APIKey = "da92d95564cf58a63cab7bac1045cfe9";
-var cityName = '';
 
+
+var citySearched = document.querySelector('#citySearched')
 
 //grabbing the recent search
 var searchBn = document.getElementById('searchBtn');
@@ -13,11 +14,13 @@ searchBn.addEventListener("click", recentSearch)
 function recentSearch() {
     var inputSrch = document.getElementById('input-search');
     var searchInput = inputSrch.value;
+    var cityName = searchInput;
+    localStorage.setItem("City Searched", JSON.stringify(cityName));
 
-    
 console.log("recent search:", searchInput);
 
 }
+
 
 
 
