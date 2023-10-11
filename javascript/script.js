e56966a85446c2427c5957b65645899db164d326
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-10-10 21:34:41 -0700

    trying to make local storage save city name upon click. then i can grab it to search lat, long by converting it then gettinthe weather.

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,16 @@
 
     //var APIKey = "da92d95564cf58a63cab7bac1045cfe9";
 
+//trying to save to loca storage upon button click not working yet.
+    function userInput() {
+      
+      var cityName = document.getElementById('input-search');
+      localStorage.setItem('inputSearch',cityName.trim().val());
+    }
 
+    
+    
+    
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=da92d95564cf58a63cab7bac1045cfe9')
     .then(function (response) {
       return response.json();
