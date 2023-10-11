f9bf7273ec4d1fedb08abc1334538852ec1d35ea
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-10-10 21:25:19 -0700

    got api working with specific coordniates. working on get it to take city then  adjust those to coordinates then fetch info based on it then return

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,18 @@
 
 
 
-    var APIKey = "da92d95564cf58a63cab7bac1045cfe9";
+    //var APIKey = "da92d95564cf58a63cab7bac1045cfe9";
 
 
-   /* var citySearched = document.querySelector('#citySearched');
-    var searchBtn = document.getElementById('searchBtn');
-    searchBtn.addEventListener("click", recentSearch);
-    
-    function recentSearch() {
-      var inputSrch = document.getElementById('input-search');
-      var searchInput = inputSrch.value;
-      var cityName = searchInput.trim();
-      
-      var existingSearches = localStorage.getItem('City Searched');
-      var cityData = existingSearches ? JSON.parse(existingSearches) : [];
-      
-      cityData.push(cityName);
-      
-      localStorage.setItem("City Searched", JSON.stringify(cityData));
-    }*/
-
-
-
-
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=da92d95564cf58a63cab7bac1045cfe9')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  
+/*
 function getApi() {
   var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=da92d95564cf58a63cab7bac1045cfe9";
   fetch(weatherUrl())
@@ -43,8 +32,7 @@ function getApi() {
 
 
 
-//recent-searches id to display form local storage and be saved to this div when searched
-
+*/
 
 //Todays Date
 var today = dayjs().format('MMM D, YYYY');
