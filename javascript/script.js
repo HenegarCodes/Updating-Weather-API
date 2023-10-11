cb3845c8fc9055e18d8544714f36ab5ef4585592
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-10-10 21:16:27 -0700

    got dayjs working. needs styling. working on getting the correct open weather api calls and fetch but not working yet

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
+
 
 
     var APIKey = "da92d95564cf58a63cab7bac1045cfe9";
 
 
-    var citySearched = document.querySelector('#citySearched');
+   /* var citySearched = document.querySelector('#citySearched');
     var searchBtn = document.getElementById('searchBtn');
     searchBtn.addEventListener("click", recentSearch);
     
@@ -19,13 +19,22 @@ $(document).ready(function() {
       cityData.push(cityName);
       
       localStorage.setItem("City Searched", JSON.stringify(cityData));
-    }
+    }*/
 
 
 
 
+function getApi() {
+  var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=da92d95564cf58a63cab7bac1045cfe9";
+  fetch(weatherUrl())
+  .then(function(response) {
+    return response.JSON();
+    })
+.then(function (data){
+  console.log(data);
 
-
+}) 
+}
 //current weather fetch
 
 
@@ -37,6 +46,9 @@ $(document).ready(function() {
 //recent-searches id to display form local storage and be saved to this div when searched
 
 
+//Todays Date
+var today = dayjs().format('MMM D, YYYY');
+document.getElementById('1a').textContent = today;
 
 
 
@@ -54,4 +66,3 @@ $(document).ready(function() {
 
 
 
-})
