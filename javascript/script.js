4b114edab256e6fac8417a1208dc9fba59c0cbcb
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-10-12 18:26:36 -0700

    still working on getting current weather to go in response. not getting it back yet

--- javascript/script.js
+++ javascript/script.js
@@ -3,14 +3,7 @@
 
     var APIkey = "da92d95564cf58a63cab7bac1045cfe9";
 
-
-//trying to save to loca storage upon button click not working yet.
-   /* function userInput() {
-      
-      var cityName = document.getElementById('input-search');
-      localStorage.setItem('input-search',cityName.trim().val());
-    }*/
-
+//get user input then use it in make weather request function and pull the lat and long from it
     var searchBtn = document.getElementById('searchBtn')
     searchBtn.addEventListener('click', handleSearch);
     function handleSearch(event) {
@@ -35,17 +28,51 @@
     })
     .then(function (data) {
       console.log(data);
+
+
+      var latitude = data[0].lat;
+      var longitude = data[0].lon;
+     
+      getWeatherData(latitude, longitude);
+      
     });
   }
 //current weather fetch
 
+function getWeatherData(latitude, longitude) {
+  fetch('http://api.openweathermap.org/geo/1.0/reverse?lat=' + latitude + '&lon=' + longitude + '&limit=1&appid=' + APIkey, {
+    method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
 
 
+  })
+}
+
+/*
 //5 day forcast fetch
+function get5DayData(latitude,longitude) {
+  fetch('api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + APIkey, {
+    method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+}
 
 
-
-
+*/
 
 //Todays Date
 var today = dayjs().format('MMM D, YYYY');
